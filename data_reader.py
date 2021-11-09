8e4eb3e3d05513184f830c47e93087949a43f1df
dorencon <dorfilfilatov@yandex.ru>
2021-11-09 16:57:36 +0300

    dr

--- data_reader.py
+++ data_reader.py
@@ -12,21 +12,23 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 import keras
 import rasterio
 
-class TrainDataRepresentation:
+'''class TrainDataRepresentation:
     def __init__(self, train_generator, steps_per_epoch, validation_data, validation_steps, callbacks, output):
         self.train_generator = train_generator
         self.steps_per_epoch = steps_per_epoch
         self.validation_data = validation_data
         self.validation_steps = validation_steps
         self.callbacks = callbacks
-        self.output = output
+        self.output = output'''
 
 class DataRepresentaion:
-    def __init__(self, data, meta, ma_exist = False, ma_data = None):
+    def __init__(self, data, meta, model_path, weight_path ma_exist = False, ma_data = None):
         self.data = data
         self.meta = meta
         if ma_exist:
             self.ma_data = ma_data
+        self.model_path = model_path
+        self.weight_path = weight_path
 
 class DataReader:
     def __init__(self, data_sourse, output, model_path, weight_path, ma_exist = False):
@@ -46,79 +48,7 @@ class DataReader:
             if (self.ma_exist):
                 zip.extract(os.path.join('manually_annotated', ''), output)
         self.data = output
-
-    '''def create_csv(self):
-        MASK_ALGORITHM = 'Kumar-Roy'
-
-        os.makedirs(os.path.join(self.data, 'csv'), exist_ok = True)
-        masks = glob.glob(os.path.join(self.data, 'mask', '*{}*.tif'.format(MASK_ALGORITHM)))
-        with open(os.path.join(self.data, 'images_masks.csv'), 'w') as f:
-            writer = csv.writer(f, delimiter = ',')
-            for mask in tqdm(masks):
-                _, mask_name = os.path.split(mask)
-                image_name = mask_name.replace('_{}_'.format(MASK_ALGORITHM), '_')
-                writer.writerow([image_name, mask_name])
-        df = pd.read_csv(os.path.join(self.data, 'images_masks.csv'))
-        images_df = df[['images']]
-        masks_df = df[['masks']]
-
-        train_ratio = 0.4
-        validation_ratio = 0.1
-        test_ratio = 0.5
-        RANDOM_STATE = 42
-
-        x_train, x_test, y_train, y_test = train_test_split(images_df, masks_df, test_size = 1 - train_ratio,
-                                                            random_state = RANDOM_STATE)
-        x_val, x_test, y_val, y_test = train_test_split(x_test, y_test,
-                                                        test_size = test_ratio/(test_ratio + validation_ratio),
-                                                        random_state=RANDOM_STATE)
-        x_train.to_csv(os.path.join(self.data, 'images_train.csv'), index=False)
-        y_train.to_csv(os.path.join(self.data, 'masks_train.csv'), index=False)
-        x_val.to_csv(os.path.join(self.data, 'images_val.csv'), index=False)
-        y_val.to_csv(os.path.join(self.data, 'masks_val.csv'), index=False)
-        x_test.to_csv(os.path.join(self.data, 'images_test.csv'), index=False)
-        y_test.to_csv(os.path.join(self.data, 'masks_test.csv'), index=False)
-
-    def get_train_data(self):
-        x_train = pd.read_csv(os.path.join(self.data, 'images_train.csv'))
-        y_train = pd.read_csv(os.path.join(self.data, 'masks_train.csv'))
-        x_val = pd.read_csv(os.path.join(self.data, 'images_val.csv'))
-        y_val = pd.read_csv(os.path.join(self.data, 'masks_val.csv'))
-        x_test = pd.read_csv(os.path.join(self.data, 'images_test.csv'))
-        y_test = pd.read_csv(os.path.join(self.data, 'masks_test.csv'))
-
-        images_train = [os.path.join(self.data, 'patches', image) for image in x_train['images']]
-        masks_train = [os.path.join(self.data, 'mask', mask) for mask in y_train['masks']]
-
-        images_validation = [os.path.join(self.data, 'patches', image) for image in x_val['images']]
-        masks_validation = [os.path.join(self.data, 'mask', mask) for mask in y_val['masks']]
-
-        RANDOM_STATE = 42
-        BATCH_SIZE = 16
-        EARLY_STOP_PATIENCE = 5
-        MODEL_NAME = 'unet'
-        MASK_ALGORITHM = 'Kumar-Roy'
-        CHECKPOINT_MODEL_NAME = 'checkpoint-{}-{}-epoch_{{epoch:02d}}.hdf5'.format(MODEL_NAME, MASK_ALGORITHM)
-        CHECKPOINT_PERIOD = 5
-
-        train_generator = generator_from_lists(images_train, masks_train, batch_size=BATCH_SIZE,
-                                               random_state=RANDOM_STATE)
-        validation_generator = generator_from_lists(images_validation, masks_validation, batch_size=BATCH_SIZE,
-                                                    random_state=RANDOM_STATE)
-
-        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE)
-        checkpoint = ModelCheckpoint(os.path.join(self.output, CHECKPOINT_MODEL_NAME), monitor='loss', verbose=1,
-                                     save_best_only=True, mode='auto', period=CHECKPOINT_PERIOD)
-
-        return TrainDataRepresentation(
-            train_generator,
-            len(images_train),
-            validation_generator,
-            len(images_validation),
-            [checkpoint, es],
-            self.output
-        )'''
-        
+     
     def create_library(self):
         self.library = []
         for i in os.walk(os.path.join(self.data, 'patches')):
@@ -136,5 +66,5 @@ class DataReader:
         meta = open(os.path.join(self.data, 'metadata', file[0] + '.txt'))
         if self.ma_exist:
             ma_data = rasterio.open(os.path.join(self.data, 'manually_annotated', self.library[index])).read().transpose((1, 2, 0))
-            return DataRepresentaion(image, meta, True, ma_data)
-        return DataRepresentaion(image, meta)
+            return DataRepresentaion(image, meta, self.model_path, self.weight_path, True, ma_data)
+        return DataRepresentaion(image, meta, self.model_path, self.weight_path)
